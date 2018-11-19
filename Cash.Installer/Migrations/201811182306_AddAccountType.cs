81264b16f57147c32fe5b3dadd35813030143cf6
Chris Lance <CLance@clearcanvasdigital.com>
2018-11-18 23:16:12 -0600

    add import package and migration to add an account type

---
+++ Cash.Installer/Migrations/201811182306_AddAccountType.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Cash.Installer.Migrations
+{
+    [Migration(201811182306)]
+    public class AddAccountType : Migration
+    {
+        public override void Up()
+        {
+            Create.Column("Type").OnTable("Account").AsAnsiString(50).NotNullable().WithDefaultValue("Ignore");
+            Execute.Sql("update Account set Type = 'Cash' where [Account Name] != 'Cash'");
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Type").FromTable("Account");
+        }
+    }
+}
