40ea9e04448a6042ea2eb52060a0505b79b4cf55
Chris Lance <CLance@clearcanvasdigital.com>
2018-11-18 21:53:54 -0600

    initial commit

---
+++ Cash.API/Controllers/QueryController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cash.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueryController : ControllerBase
+    {
+        private const string ConnectionString = @"data source=.\sql2014; initial catalog = Cash; persist security info = True; user id=web; password=web;";
+
+        [HttpPost("")]
+        public async Task<IActionResult> RunQuery([FromBody] QueryModel query, CancellationToken cancellationToken)
+        {
+            var result = new List<List<string>>();
+
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                await conn.OpenAsync(cancellationToken);
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = query.Query;
+                    command.CommandType = CommandType.Text;
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync(cancellationToken))
+                    {
+                        int fieldCount = reader.FieldCount;
+
+                        while (await reader.ReadAsync(cancellationToken))
+                        {
+                            var row = new List<string>();
+                            result.Add(row);
+                            for (int i = 0; i < fieldCount; i++)
+                            {
+                                row.Add(reader[i] == DBNull.Value ? null : reader[i].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+            return Ok(new ResponseModel {Data = result});
+        }
+    }
+
+    public class QueryModel
+    {
+        public string Query { get; set; }
+
+        public string[] Parameters { get; set; }
+    }
+
+    public class ResponseModel
+    {
+        public List<List<string>> Data { get; set; }
+    }
+}
---
+++ Cash.API/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace Cash.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            ConfigureExceptionHandler(app);
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseStaticFiles();
+        }
+
+        private void ConfigureExceptionHandler(IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        Console.Error.WriteLine($"Something went wrong: {contextFeature.Error}");
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                        {
+                            message = contextFeature.Error.Message
+                        }));
+                    }
+                });
+            });
+        }
+    }
+}
